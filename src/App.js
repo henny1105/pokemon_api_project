657e97cb6a4ec62d6d963216890afb2fd27511a4
sinhey <sinheyyy@gmail.com>
2024-04-15 23:10:25 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          sinhey branch test
         </a>
       </header>
     </div>
