13faa6ce446d0bbf3f3493e8cf16ac4b2a5ff957
henny <kimhyein1105@gmail.com>
2024-04-16 17:15:20 +0900

    초기 셋팅 진행중

---
+++ src/pages/common/FloatingCursor.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+const FloatingCursor = ({ imgSrc, altText }) => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updatePosition = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener('mousemove', updatePosition);
+
+    return () => {
+      window.removeEventListener('mousemove', updatePosition);
+    };
+  }, []);
+
+  return (
+    <div className="cursor" style={{ left: `${position.x}px`, top: `${position.y}px` }}>
+      <img src={imgSrc} alt={altText} />
+    </div>
+  );
+};
+
+export default FloatingCursor;
--- src/pages/random/Random.jsx
+++ src/pages/random/Random.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import './Random.css';
+import FloatingCursor from '../common/FloatingCursor';
+
+// -랜덤으로 뽑은 포켓몬은 나의 포켓몬에 저장된다
+// -랜덤 포켓몬은 진화되지 않은 포켓몬만 나온다?
+// -랜덤 뽑기시 오박사의 오늘의 포켓몬은 뭘까요?처럼 검은색 그림자로 먼저 포켓몬이 나온다
+// -뽑기는 티켓을 사용해서 뽑느다
+// -티켓은 1분에 하나씩 차오른다
+// -티켓의 최대치는 20개이다
+// -나의 포켓몬에 저장되어있는 포켓몬이 나오면 이상한사탕으로 변한다
+// -랜덤 뽑기시 뮤,뮤츠 같은 종류의 확률은 낮게 설정한다
+// -테스트 용으로 티켓 최대치를 채우는 버튼을 숨겨둔다
 
 const Random = () => {
-  return (
-    <div>
-      랜덤 페이지
-    </div>
-  )
-}
-
-export default Random
+    return (
+     <>
+      <FloatingCursor imgSrc="/img/random/pokeball.svg" altText="Pokeball" />
+      <div className='random_page'>
+        <div className="inner">
+          랜덤 페이지
+        </div>
+      </div>
+     </>
+    );
+  };
+  
+export default Random;
