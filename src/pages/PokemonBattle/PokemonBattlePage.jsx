4bffd5d9fce55ba669cf8fbc418568950b851a5d
sinhey <sinheyyy@gmail.com>
2024-04-16 21:59:10 +0900

    배틀 페이지 - 배틀 카드 추가, 배경 설정

--- src/pages/PokemonBattle/PokemonBattlePage.jsx
+++ src/pages/PokemonBattle/PokemonBattlePage.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import PokemonBattleCard from './components/PokemonBattleCard/PokemonBattleCard'
+import "./PokemonBattlePage.style.css"
 
 const PokemonBattlePage = () => {
   return (
-    <div>
+    <div className='battle-backgorund' style={{
+      backgroundImage: "url(" + `https://podic.kr/images/misc/Natural_Green_Berry_Tree.png` + ")"
+    }}>
+      <div>
+        <PokemonBattleCard/>
+      </div>
+      <div>
+        <PokemonBattleCard />
+      </div>
       Battle
+
     </div>
   )
 }
