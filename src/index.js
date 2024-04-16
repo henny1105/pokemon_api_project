37d911200d56606572a3dabb8a903aea44f0df2c
moojaa <129938297+moojaa@users.noreply.github.com>
2024-04-16 15:07:01 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { Provider } from 'react-redux'; 
+import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'; 
-import store from './redux/store';
 import MyPokemonPage from './page/mypokemonpage/MyPokemonPage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    <React.StrictMode>
-    {/* <App /> */}
+      {/* <App /> */}
       <MyPokemonPage />
-    </React.StrictMode>
+    </BrowserRouter>
   </Provider>
 );
 
