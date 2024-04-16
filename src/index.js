a09ed5148af293a1e867e344b6cba6ab6a892966
moojaa <129938297+moojaa@users.noreply.github.com>
2024-04-16 23:32:43 +0900

    성장페이지

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
+import MyPokemonPage from './page/mypokemonpage/MyPokemonPage'
+// import RaisePage from './page/raisepage/RaisePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      {/* <App /> */}
+      <MyPokemonPage />
+      {/* <RaisePage/> */}
     </BrowserRouter>
   </Provider>
 );
