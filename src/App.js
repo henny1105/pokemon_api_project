61cb468b13195db36df03f1ca36868b0758ea264
kimHyeonJu <kimhyesom1202@gmail.com>
2024-04-17 23:05:38 +0900

    ✨ package 설치 안된 것 + router 설정

--- src/App.js
+++ src/App.js
@@ -5,17 +5,28 @@ import AppLayout from './page/AppLayout/AppLayout';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './page/home/Home';
 import Random from './page/random/Random';
+import PokemonDex from './page/pokemondex/PokemonDex';
+import PokemonInfo from './page/pokemondex/components/pokemoninfo/PokemonInfo';
+import RaisePage from './page/raisepage/RaisePage';
+import MyPokemonPage from './page/mypokemonpage/MyPokemonPage';
+import PokemonBattle from './page/PokemonBattle/PokemonBattlePage';
 import './App.css';
-
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<AppLayout/>}>
-        <Route path='/' element={<Home/>}/>
-				<Route path='random' element={<Random />} />
+        <Route index element={<Home/>}/>
+        <Route path='/random' element={<Random />} />
+        <Route path="/pokemondex">
+          <Route index element={<PokemonDex />} />
+          <Route path="pokemoninfo/:id" element={<PokemonInfo />} />
+        </Route>
+        <Route path="/mypokemon" element={ <MyPokemonPage /> }>
+          <Route path=":id" element={ <RaisePage />}/>
+        </Route>
+        <Route path="/battle" element={ <PokemonBattle />} />
       </Route>
-
       {/* <Route path="*" element={<NotFoundPage/>}/> */}
     </Routes>
   )
