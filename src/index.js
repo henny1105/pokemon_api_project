cf1e67a3bf28f69e3f37a7e16002124b76f610f1
moojaa <129938297+moojaa@users.noreply.github.com>
2024-04-17 21:31:38 +0900

    index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
-import MyPokemonPage from './page/mypokemonpage/MyPokemonPage'
-// import RaisePage from './page/raisepage/RaisePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      {/* <App /> */}
-      <MyPokemonPage />
-      {/* <RaisePage/> */}
+      <App />
     </BrowserRouter>
   </Provider>
 );
