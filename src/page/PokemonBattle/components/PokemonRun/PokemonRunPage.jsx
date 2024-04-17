dbea2157147eeaa17c5f8683ccf6c14402ba1d1c
sinhey <sinheyyy@gmail.com>
2024-04-18 01:38:07 +0900

    도망 페이지 추가

--- src/page/PokemonBattle/components/PokemonRun/PokemonRunPage.jsx
+++ src/page/PokemonBattle/components/PokemonRun/PokemonRunPage.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import "./PokemonRunPage.style.css"
+import '@kfonts/neodgm-code';
+import { useNavigate } from 'react-router-dom/dist';
 
 const PokemonRunPage = () => {
+    const navigate = useNavigate();
+
+    const goToHome = () => {
+        navigate("/");
+    }
+
     return (
-        <div>
-            도망
+        <div id="run" className='run-backgorund' style={{
+            backgroundImage: "url(" + `https://podic.kr/images/misc/Natural_Green_Berry_Tree.png` + ")"
+        }}>
+            <div className="run-space"></div>
+            <div className='run-message-container'>
+                <div className='run-message'>
+                    <div style={{ marginBottom: 10 }}>성공적으로 도망쳤다...</div>
+                    <div className='run-btns'>
+                        <button className='run-home-btn' onClick={() => goToHome()}>돌아간다.</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
