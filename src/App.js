dbf5aefff78a9ec93899a01dec0b14128787c842
kimHyeonJu <kimhyesom1202@gmail.com>
2024-04-17 22:23:11 +0900

    🚚 Router 변경

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 // > router
 import { Routes, Route } from 'react-router-dom';
 // > components
-import Home from './pages/Home';
+// import Home from './pages/Home';
 import PokemonDex from './pages/pokemondex/PokemonDex';
 import PokemonInfo from './pages/pokemondex/components/pokemoninfo/PokemonInfo';
+import PokeApi from './page/PokeApi';
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={ <Home /> }/>
+        <Route path="/" element={ <PokeApi /> }/>
         <Route path="pokemondex">
           <Route index element={<PokemonDex />} />
           <Route path="pokemoninfo/:id" element={<PokemonInfo />} />
