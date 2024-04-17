24cb8f55ae1c87a77860d6d90e861d74466db390
kimHyeonJu <kimhyesom1202@gmail.com>
2024-04-17 19:24:43 +0900

    ✨ router 를 위한 App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+// > router
+import { Routes, Route } from 'react-router-dom';
+// > components
+import Home from './pages/Home';
+import PokemonDex from './pages/pokemondex/PokemonDex';
+import PokemonInfo from './pages/pokemondex/components/pokemoninfo/PokemonInfo';
 
 const App = () => {
   return (
     <div>
-      김현주입니다.
+      <Routes>
+        <Route path="/" element={ <Home /> }/>
+        <Route path="pokemondex">
+          <Route index element={<PokemonDex />} />
+          <Route path="pokemoninfo/:id" element={<PokemonInfo />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
