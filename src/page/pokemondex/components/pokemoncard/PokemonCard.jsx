b11e9db63f7ac8d675d929aec427d3b20e0844ec
kimHyeonJu <kimhyesom1202@gmail.com>
2024-04-18 22:38:28 +0900

    ✨ pokemondex search 구현

--- src/page/pokemondex/components/pokemoncard/PokemonCard.jsx
+++ src/page/pokemondex/components/pokemoncard/PokemonCard.jsx
@@ -1,10 +1,30 @@
 import React, { useState } from 'react';
 import './PokemonCard.style.css';
-const PokemonCard = ({ pokemonData, movePokemonInfo }) => {
+const PokemonCard = ({ pokemonData, movePokemonInfo, filtered }) => {
   const [catchPokemon] = useState(false); 
   return (
     <>
     {
+      filtered
+      ?
+      filtered?.map((item, index) => (
+        <div className="card drop_shadow_2" key={index} onClick={ () => movePokemonInfo(index+1) }>
+          <div className="card_number">
+            <span>#{ item.id }</span>
+          </div>
+          <div className="card_img is_exist">
+            {
+              catchPokemon === true
+              ?
+              <img src={ item.image } alt="" className="catch" />
+              :
+              <img src={ item.image } alt="" className="non_catch" />
+            }
+          </div>
+          <div className="card_name">{ item.korean_name }</div>
+        </div>
+      ))
+      :
       pokemonData?.map((item, index) => (
         <div className="card drop_shadow_2" key={index} onClick={ () => movePokemonInfo(index+1) }>
           <div className="card_number">
