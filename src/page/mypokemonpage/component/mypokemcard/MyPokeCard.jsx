18c77b093e5532c5c56619e2387ff0313ad6ffd6
moojaa <129938297+moojaa@users.noreply.github.com>
2024-04-18 21:34:15 +0900

    슬라이스 수정

--- src/page/mypokemonpage/component/mypokemcard/MyPokeCard.jsx
+++ src/page/mypokemonpage/component/mypokemcard/MyPokeCard.jsx
@@ -10,17 +10,15 @@ const MyPokeCard = ({myPoke}) => {
     useEffect(() => {
       const fetchPokeData = async () => {
         try {
-          const response = await axios.get(myPoke.url);
+          const response = await axios.get(myPoke.data.url);
           const speciesResponse = await axios.get(response.data.species.url);
           const data = response.data;
           const koreanName = speciesResponse.data.names.find(
             (name) => name.language.name === "ko"
           );
   
-          // 원하는 데이터로부터 이미지 URL을 추출하는 등의 작업 수행
           const imageUrl = data.sprites?.versions['generation-v']['black-white'].animated.front_default;
   
-          // 추출한 이미지 URL을 상태에 설정
           setPokeImg(imageUrl);
           setPokeId(data.id)
           setPokeName(koreanName ? koreanName.name :'unknown')
@@ -31,9 +29,9 @@ const MyPokeCard = ({myPoke}) => {
         }
       };
   
-      // myPoke.url이 변경될 때마다 데이터를 다시 가져옴
       fetchPokeData();
     }, [myPoke.url]);
+
     return (
         <div className='sun-pokemon-card-area body_3'>
           <div className='sun-card-number'>{`no.${pokeId}`}</div>
